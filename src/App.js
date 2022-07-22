46c230835835083d0bd2b7ea22c875150465d141
Eliel Oliveira <elielsjc@hotmail.com>
2022-07-22 11:49:55 -0300

    iniciando reformulação

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import './styles/Header.css'
+import React from 'react'
+import Main from './pages/Main'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App () {
+  return (
+    <>
+      <Header/>
+      {/* <Main/>
+      <Footer/> */}
+    </>
+  )
+}
+
+export default App
